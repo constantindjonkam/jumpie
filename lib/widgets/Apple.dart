87fdfa55b54eac8ffada275f97f24438f817f7ea
Constantin Djonkam <fokouaconstantin@gmail.com>
2021-08-23 03:22:22 -0400

    the apple widget with defined width and get alignments as props

---
+++ lib/widgets/Apple.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Apple extends StatelessWidget {
+  final double appleX;
+  final double appleY;
+
+  Apple(this.appleX, this.appleY);
+
+  @override
+  build(BuildContext context) {
+    return Container(
+      alignment: Alignment(appleX, appleY),
+      child: Container(
+        // height: 35,
+        width: 35,
+        child: Image.asset("assets/images/apple.png"),
+      ),
+    );
+  }
+}
